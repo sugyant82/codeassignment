0b8f098842a4c1a35201bb32a2dec71232f44fcf
Sugs <sugyant@gmail.com>
2019-04-08 00:43:44 +0800

    Create Solution3.java
    Solution 3

---
+++ code_assignment/src/code_assignment/Solution3.java
@@ -0,0 +1,19 @@
+package code_assignment;
+
+
+public class Solution3 {
+
+	public static void main (String[] args) {
+
+
+		System.out.println("A rooster say: ");
+		AnimalFactory.getSpecialBird("Rooster").sing();
+
+
+		//Both chickens and roosters are special birds wich cannot Fly.
+
+	}
+
+
+
+}
