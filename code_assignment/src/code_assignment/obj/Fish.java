aad5196484a82e5980143fc8764e9618a01a6b3c
sugyant82 <49311090+sugyant82@users.noreply.github.com>
2019-04-08 14:50:03 +0800

    Solution 5
    added Fish class concrete object

---
+++ code_assignment/src/code_assignment/obj/Fish.java
@@ -0,0 +1,12 @@
+package code_assignment.obj;
+
+import code_assignment.abs.AFish;
+
+public class Fish extends AFish {
+	
+	public void swim() {
+		// TODO Auto-generated method stub
+		System.out.println("swim swim swim!");
+	}
+
+}
